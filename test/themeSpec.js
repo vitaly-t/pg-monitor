a09d741bd50f1a7cbd3bb8d33e17e20848f35e3d
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-19 20:04:53 +0000

    adding tests.

--- test/themeSpec.js
+++ test/themeSpec.js
@@ -7,5 +7,18 @@ describe("Theme - Positive", function () {
 });
 
 describe("Theme - Negative", function () {
-
+    describe("invalid parameters", function () {
+        it("must throw an error", function () {
+            expect(function () {
+                mon.setTheme();
+            }).toThrow('Invalid theme parameter specified.');
+        });
+    });
+    describe("non-existing theme", function () {
+        it("must throw an error", function () {
+            expect(function () {
+                mon.setTheme('unknown');
+            }).toThrow("Theme 'unknown' does not exist.");
+        });
+    });
 });
