39567f59524c705b6427438d982541289e62ab1e
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-20 01:45:42 +0000

    adding tests.

--- test/events/errorSpec.js
+++ test/events/errorSpec.js
@@ -3,7 +3,30 @@
 var mon = require("../../lib");
 
 describe("Error - Positive", function () {
-
+    describe("within transaction", function () {
+        var options = {}, text = [];
+        var e = {
+            query: "hello",
+            ctx: {
+                start: new Date(),
+                tag: "test"
+            }
+        };
+        beforeEach(function () {
+            mon.attach(options, ['error']);
+            mon.log = function (msg, info) {
+                text.push(info.text);
+                info.display = false;
+            };
+            options.error("errMsg", e);
+        });
+        it("must be successful", function () {
+            expect(text).toEqual(['error: errMsg', 'task(test): hello']);
+        });
+        afterEach(function () {
+            mon.log = null;
+        });
+    });
 });
 
 describe("Error - Negative", function () {
--- test/events/querySpec.js
+++ test/events/querySpec.js
@@ -3,7 +3,30 @@
 var mon = require("../../lib");
 
 describe("Query - Positive", function () {
-
+    describe("within transaction", function () {
+        var options = {}, text;
+        var e = {
+            query: "hello",
+            ctx: {
+                start: new Date(),
+                tag: "test"
+            }
+        };
+        beforeEach(function () {
+            mon.attach(options, ['query']);
+            mon.log = function (msg, info) {
+                text = info.text;
+                info.display = false;
+            };
+            options.query(e);
+        });
+        it("must be successful", function () {
+            expect(text).toBe('task(test): hello');
+        });
+        afterEach(function () {
+            mon.log = null;
+        });
+    });
 });
 
 describe("Query - Negative", function () {
