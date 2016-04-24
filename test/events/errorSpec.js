d1b3da38cdc5ee4fb89fe2cd9ad47655589e0b56
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-04-24 22:40:54 +0100

    improving support for prepared statements.

--- test/events/errorSpec.js
+++ test/events/errorSpec.js
@@ -82,6 +82,50 @@ describe("Error - Positive", function () {
         });
     });
 
+    describe("query is a prepared statement with params", function () {
+        var context = {
+            query: {name: 'test-name', text: 'text-text', values: [123]}
+        };
+        var options = {}, text = [];
+        beforeEach(function () {
+            mon.attach(options, ['error']);
+            mon.log = function (msg, info) {
+                text.push(info.text);
+                info.display = false;
+            };
+            options.error("errMsg", context);
+        });
+        it("must parse the value", function () {
+            expect(text).toEqual(['error: errMsg', 'query: {"name":"test-name","text":"text-text","values":[123]}']);
+        });
+        afterEach(function () {
+            mon.detach();
+            mon.log = null;
+        });
+    });
+
+    describe("query is a prepared statement without params", function () {
+        var context = {
+            query: {name: 'test-name', text: 'text-text'}
+        };
+        var options = {}, text = [];
+        beforeEach(function () {
+            mon.attach(options, ['error']);
+            mon.log = function (msg, info) {
+                text.push(info.text);
+                info.display = false;
+            };
+            options.error("errMsg", context);
+        });
+        it("must parse the value", function () {
+            expect(text).toEqual(['error: errMsg', 'query: {"name":"test-name","text":"text-text"}']);
+        });
+        afterEach(function () {
+            mon.detach();
+            mon.log = null;
+        });
+    });
+
     describe("connection", function () {
         var context = {
             cn: 123
