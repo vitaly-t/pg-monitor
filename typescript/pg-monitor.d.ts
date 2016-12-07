2731a036f4d2a27f89c7312bbc919b845d3d0494
Mauricio Vigolo <mauriciovigolo@gmail.com>
2016-12-06 23:14:54 -0200

    remove needless module declaration

--- typescript/pg-monitor.d.ts
+++ typescript/pg-monitor.d.ts
@@ -21,34 +21,30 @@ interface IEventInfo {
     display: boolean;
 }
 
-export module pgMonitor {
+export function attach(options: Object, events?: Array<string>, override?: boolean): void;
 
-    export function attach(options: Object, events?: Array<string>, override?: boolean): void;
+export function attach(options: {
+    options: Object,
+    events?: Array<string>,
+    override?: boolean
+}): void;
 
-    export function attach(options: {
-        options: Object,
-        events?: Array<string>,
-        override?: boolean
-    }): void;
+export function detach(): void;
 
-    export function detach(): void;
+export function setTheme(theme: string | IColorTheme): void;
 
-    export function setTheme(theme: string|IColorTheme): void;
+export function log(msg: string, info: IEventInfo): void;
 
-    export function log(msg: string, info: IEventInfo): void;
+export var detailed: boolean;
 
-    export var detailed: boolean;
+export function connect(client: Object, dc: any, fresh: boolean, detailed?: boolean): void;
 
-    export function connect(client: Object, dc: any, fresh: boolean, detailed?: boolean): void;
+export function disconnect(client: Object, dc: any, detailed?: boolean): void;
 
-    export function disconnect(client: Object, dc: any, detailed?: boolean): void;
+export function query(e: Object, detailed?: boolean): void;
 
-    export function query(e: Object, detailed?: boolean): void;
+export function task(e: Object): void;
 
-    export function task(e: Object): void;
+export function transact(e: Object): void;
 
-    export function transact(e: Object): void;
-
-    export function error(err: any, e: Object, detailed?: boolean): void;
-
-}
+export function error(err: any, e: Object, detailed?: boolean): void;
